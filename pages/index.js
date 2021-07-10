fe387569ac8d82edc8b2d289745231eed42eca37
katherinepeterson <katherine.peterson@yahoo.com>
2021-07-10 11:21:01 -0700

    change head title

--- pages/index.js
+++ pages/index.js
@@ -5,36 +5,38 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Spaces Promo</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-      <div className="max-w-xl mx-auto">
-        <form className="space-y-8 divide-y divide-gray-200">
-          <div className="space-y-8 divide-y divide-gray-200">
-            <div className="pt-8">
-              <h3 className="text-lg font-medium leading-6 text-gray-900">Twitter Space Details</h3>
-              <div className="grid grid-cols-1 mt-6 gap-y-6 gap-x-4 sm:grid-cols-6">
-                <div className="col-span-full">
-                  <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
-                    Title
-                  </label>
-                  <div className="mt-1">
-                    <input
-                      type="text"
-                      name="title"
-                      id="title"
-                      className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-50text-sm"
-                    />
+        <div className="max-w-xl mx-auto">
+          <form className="space-y-8 divide-y divide-gray-200">
+            <div className="space-y-8 divide-y divide-gray-200">
+              <div className="pt-8">
+                <h3 className="text-lg font-medium leading-6 text-gray-900">
+                  Twitter Space Details
+                </h3>
+                <div className="grid grid-cols-1 mt-6 gap-y-6 gap-x-4 sm:grid-cols-6">
+                  <div className="col-span-full">
+                    <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                      Title
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-50text-sm"
+                      />
+                    </div>
                   </div>
+                  <SpeakerSearch />
                 </div>
-                <SpeakerSearch />
               </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   )
 }
