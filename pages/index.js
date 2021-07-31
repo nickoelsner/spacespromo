cb6d164d3d6618c146c60880a68f387990b0a546
Nick Oelsner <noelsner@gmail.com>
2021-07-30 19:01:17 -0700

    added gradient background options, additional text color options

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import SpeakerSearch from '../components/SpeakerSearch'
 import { useState } from 'react'
 import Head from 'next/head'
 import * as htmlToImage from 'html-to-image'
+import { width } from 'tailwindcss/defaultTheme'
 const download = require('downloadjs')
 
 const backgroundColors = [
@@ -11,9 +12,12 @@ const backgroundColors = [
   'bg-[#EE6A65]',
   'bg-[#F5DC66]',
   'bg-[#E47DF8]',
+  'bg-gradient-to-br from-pink-500 to-indigo-800',
+  'bg-gradient-to-br from-blue-200 to-blue-700',
+  'bg-gradient-to-br from-teal-100 to-teal-400',
 ]
 
-const textColors = ['text-white', 'text-gray-900']
+const textColors = ['text-white', 'text-gray-200', 'text-gray-700', 'text-gray-900']
 
 export default function Home() {
   const [speakers, setSpeakers] = useState([])
@@ -23,10 +27,38 @@ export default function Home() {
   const [dateTime, setDateTime] = useState('')
 
   const onSaveImage = () => {
+    const style = { width: '1150px', height: '646.875px' }
+    const height = '646.875px'
+    const width = '1150px'
     htmlToImage.toPng(document.getElementById('promo-image')).then(function (dataUrl) {
-      download(dataUrl, 'my-node.png')
+      download(dataUrl, 'SpacesPromo.png')
     })
   }
+
+  const ImagePreview = ({ id }) => (
+    <div
+      id={id}
+      className={`w-[575px] h-[323.4375px] ${backgroundColor} flex flex-col justify-between items-center p-5 mb-10`}
+    >
+      <h1 className={`mb-5 text-xl font-medium text-center ${textColor}`}>{title || 'Title'}</h1>
+      <div className="flex-grow" />
+      <ul className="flex flex-wrap justify-center">
+        {speakers.map((speaker) => (
+          <li className="flex flex-col items-center mx-2 mb-3" key={speaker.name}>
+            <img
+              className="w-[4.9rem] h-[4.9rem] mx-auto rounded-full"
+              src={`https://unavatar.io/twitter/${speaker.username}`}
+              alt=""
+            />
+            <h3 className={`text-[12px] font-medium ${textColor}`}>{speaker.name}</h3>
+          </li>
+        ))}
+      </ul>
+      <div className="flex-grow" />
+      <div className="flex-grow" />
+      <p className={`text-center ${textColor}`}>{dateTime}</p>
+    </div>
+  )
 
   return (
     <div>
@@ -80,49 +112,32 @@ export default function Home() {
                 {backgroundColors.map((color, i) => (
                   <button
                     key={i}
-                    className={`w-12 h-5 mr-2 ${color} ${
+                    className={`w-14 h-8 mr-2 shadow-md ${color} ${
                       backgroundColor === color ? 'w-14 h-6' : ''
                     }`}
                     onClick={() => setBackgroundColor(color)}
                   />
                 ))}
                 <h4 className="block mb-1 text-sm font-medium text-gray-700">Text Color</h4>
-                {textColors.map((color, i) => (
-                  <button
-                    key={i}
-                    className={`w-12 h-5 mr-2 ${
-                      color === 'text-white' ? 'bg-white shadow-md' : 'bg-gray-900'
-                    } ${textColor === color ? 'w-14 h-6' : ''}`}
-                    onClick={() => setTextColor(color)}
-                  />
-                ))}
+                {textColors.map((color, i) => {
+                  const bgColor = color.replace(/text-/g, '')
+                  console.log('typeOf(bgColor) :>> ', typeof bgColor)
+                  console.log('color :>> ', color)
+                  console.log('bgColor :>> ', bgColor)
+                  return (
+                    <button
+                      key={i}
+                      className={`w-14 h-8 mr-2 shadow-md bg-${bgColor} ${
+                        textColor === color ? 'w-14 h-6' : ''
+                      }`}
+                      onClick={() => setTextColor(color)}
+                    />
+                  )
+                })}
                 <h3 className="pt-8 mb-4 text-lg font-medium leading-6 text-gray-900">
                   Image Preview
                 </h3>
-                <div
-                  id="promo-image"
-                  className={`w-[575px] h-[323.4375px] ${backgroundColor} flex flex-col justify-between items-center p-5 mb-10`}
-                >
-                  <h1 className={`mb-5 text-xl font-medium text-center ${textColor}`}>
-                    {title || 'Title'}
-                  </h1>
-                  <div className="flex-grow" />
-                  <ul className="flex flex-wrap justify-center">
-                    {speakers.map((speaker) => (
-                      <li className="flex flex-col items-center mx-2 mb-3" key={speaker.name}>
-                        <img
-                          className="w-[4.9rem] h-[4.9rem] mx-auto rounded-full"
-                          src={`https://unavatar.io/twitter/${speaker.username}`}
-                          alt=""
-                        />
-                        <h3 className={`text-[12px] font-medium ${textColor}`}>{speaker.name}</h3>
-                      </li>
-                    ))}
-                  </ul>
-                  <div className="flex-grow" />
-                  <div className="flex-grow" />
-                  <p className={`text-center ${textColor}`}>{dateTime}</p>
-                </div>
+                <ImagePreview id="promo-image" />
                 <button onClick={onSaveImage}>Save Image</button>
               </div>
             </div>
