b255b11d68a01cf87e4f80df3bfa3b521227f229
Nick Oelsner <noelsner@gmail.com>
2021-08-09 09:29:13 -0700

    remove auth0, add compose tweet button

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
 import '../styles/globals.css'
-import { UserProvider } from '@auth0/nextjs-auth0'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
-  )
+  return <Component {...pageProps} />
 }
